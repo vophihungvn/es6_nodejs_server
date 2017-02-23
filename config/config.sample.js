6eba73522c76af585eec4f255e82f0ace3435ea1
Vo Phi Hung <vphungkhtn@gmail.com>
2017-02-23 16:25:39 +0700

    Fix config and .gitignore

--- config/config.sample.js
+++ config/config.sample.js
@@ -6,13 +6,6 @@ module.exports = {
     port: 9000,
     limit: 100
   },
-  database: {
-    host     : '127.0.0.1',
-    user     : 'postgres',
-    password : '123456',
-    database : 'es6-server',
-    charset  : 'utf8'
-  },
   auth: {
     encrypt_key: 'es6-server',
     expire: 7*24*60*60*1000 //7 days
---
+++ config/database.sample.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "postgres",
+    "password": null,
+    "database": "database_name",
+    "host": "localhost",
+    "dialect": "postgresql"
+  },
+  "test": {
+    "username": "postgres",
+    "password": null,
+    "database": "database_name",
+    "host": "localhost",
+    "dialect": "postgresql"
+  },
+  "production": {
+    "username": "postgres",
+    "password": null,
+    "database": "database_name",
+    "host": "localhost",
+    "dialect": "postgresql"
+  }
+}
